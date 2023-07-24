21c434b4f241afa4284ac9b51e371424c4f9389e
Skeller1z <kaktv1333@gmail.com>
2023-07-25 01:28:56 +0700

    update

--- src/src-sw.js
+++ src/src-sw.js
@@ -1,9 +1,60 @@
 /* eslint-disable no-restricted-globals */
 import { clientsClaim } from "workbox-core";
 import { precacheAndRoute } from "workbox-precaching";
+import {registerRoute} from 'workbox-routing'
+import {CacheFirst, StaleWhileRevalidate} from 'workbox-strategies'
+import {CacheableResponsePlugin} from 'workbox-cacheable-response'
+import { ExpirationPlugin } from "workbox-expiration";
 
 clientsClaim();
 
 self.skipWaiting();
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+    ({url}) => url.origin === 'https://fonts.googleapis.com',
+    new StaleWhileRevalidate({
+        cacheName: 'google-fonts-stylesheets',
+    })
+);
+
+registerRoute(
+    ({url}) => url.origin === 'https://fonts.gstatic.com',
+    new StaleWhileRevalidate({
+        cacheName: 'google-fonts-webfonts',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new ExpirationPlugin({
+                maxAgeSeconds: 60 * 60 * 24 *365,
+                maxEntries: 30,
+            }),
+        ],
+    }),
+);
+
+registerRoute(
+    ({request}) => request.destination === 'image',
+    new CacheFirst({
+        cacheName: 'images',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new ExpirationPlugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 *60,
+            }),
+        ],
+    })
+);
+
+registerRoute(
+    ({request}) => request.destination === 'script' ||
+    request.destination === 'style',
+    new StaleWhileRevalidate({
+        cacheName: 'static-resource',
+    })
+);
