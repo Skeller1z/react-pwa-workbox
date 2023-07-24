5f7dfe95b8b2f5a12917db3bd7e0f378dd71d28f
Skeller1z <kaktv1333@gmail.com>
2023-07-24 17:27:52 +0700

    first commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const home = (props: Props) => {
+  return (
+    <div>this is home components</div>
+  )
+}
+
+export default home
---
+++ src/src-sw.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-restricted-globals */
+import { clientsClaim } from "workbox-core";
+import { precacheAndRoute } from "workbox-precaching";
+
+clientsClaim();
+
+self.skipWaiting();
+
+precacheAndRoute(self.__WB_MANIFEST);
